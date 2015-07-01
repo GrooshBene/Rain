639ec34d984ed982deac179703449dd27663ca92
GrooshBene <wltn000129@gmail.com>
2015-07-02 00:29:23 +0900

    Dialog Layout Done

--- app/src/main/java/onegreat/rain/Home.java
+++ app/src/main/java/onegreat/rain/Home.java
@@ -35,7 +35,7 @@ public class Home extends Activity {
         setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"오예 섹스",Toast.LENGTH_SHORT).show();
+
             }
         });
     }
