a849d2808766731dae9aa85d4ae2495d343423f1
GrooshBene <wltn000129@gmail.com>
2015-07-03 21:50:29 +0900

    Resource PreView Removed, Custom Dialog making

---
+++ app/src/main/java/onegreat/rain/CustomDialog.java
@@ -0,0 +1,14 @@
+package onegreat.rain;
+
+import android.app.Dialog;
+import android.content.Context;
+
+/**
+ * Created by GrooshBene on 2015-07-03.
+ */
+public class CustomDialog extends Dialog{
+    private CustomDialog mCustomDialog;
+    public CustomDialog(Context context) {
+        super(context);
+    }
+}
